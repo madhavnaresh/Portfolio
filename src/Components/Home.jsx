dc3b99d49570000f4b9fe79280df33e8fef19d41
Madhav Gupta <madhavgupta639@gmail.com>
2024-07-03 09:44:05 +0000

    my-image

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -22,8 +22,8 @@ import PropTypes from "prop-types";
  */
 import image from "../images/my_image.jpg";
 
-//const imageAltText =
-//"Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText =
+  "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
 
 const Home = ({ name, title }) => {
   return (
