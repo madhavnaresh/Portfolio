fa01f5299a49f825b167446ee5d062ecdac71756
Madhav Gupta <madhavgupta639@gmail.com>
2024-07-03 08:29:39 +0000

    added project

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,26 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "E-commerce Website 🎉",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Created a beginner friendly E-commerce website to practice and developed it using HTML and CSS only",
+    url: "https://github.com/madhavnaresh/Cart",
   },
   {
-    title: "Web Development for Beginners",
-    description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+    title: "BG-Color changer",
+    description: "JavaScript also Used here, remaining all I've done using Frontend Basics",
+    url: "https://github.com/madhavnaresh/BG-Color-changer",
   },
   {
-    title: "My Resume Site",
+    title: "My other Portfolio Site",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Created simple portfolio website using CSS and HTML.It ncludes my experience and design abilities.",
+    url: "https://github.com/madhavnaresh/Portfolio",
   },
   {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    title: "Learning JavaScript Basics",
+    description: "Right now, I'm using Geekster to learn JavaScript.",
+    url: "https://github.com/madhavnaresh/JavaScript",
   },
 ];
 
